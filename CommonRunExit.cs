7a415ff8ec1ed0c2ef91e0c7c51629eac4fb690f
红色枫叶 <75351663@qq.com>
2019-08-20 14:40:47 +0800

    add

---
+++ CommonRunExit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maple.NetCore
+{
+    public class CCommonRunExit : IDisposable
+    {
+        Action ActionExit { get; }
+        public CCommonRunExit(Action begin, Action exit)
+        {
+            begin.Invoke();
+            ActionExit = exit;
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            ActionExit.Invoke();
+        }
+    }
+
+
+
+}
